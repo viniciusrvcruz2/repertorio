0710d13ef4a8e79754f9b2440b70c6c934c7b68a
viniciusrvcruz2 <viniciusrobertopro2@gmail.com>
2024-02-17 16:31:11 -0300

    fix: corrections

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Repertoires } from "../pages/repertoires/Repertoires";
 import { Login } from "../pages/login/Login";
 import { PrivateRoutes } from "./PrivateRoutes";
-import { Form } from "../pages/form/Form";
 import { AuthContext } from "../contexts/AuthContext";
 import { useContext } from "react";
 import { RepertoireMusics } from "../pages/repertoireMusics/RepertoireMusics";
@@ -17,7 +16,6 @@ export const RoutesApp = () => {
             <Route path="/" element={<PrivateRoutes><Repertoires /></PrivateRoutes>} />
             <Route path="/repertoire/:repertoireId" element={<PrivateRoutes><RepertoireMusics /></PrivateRoutes>} />
             <Route path="/repertoire/:repertoireId/:musicId" element={<PrivateRoutes><Music /></PrivateRoutes>} />
-            <Route path="/form" element={<PrivateRoutes><Form /></PrivateRoutes>} />
             <Route path="/login" element={!user ? <Login /> : <Navigate to='/' />} />
         </Routes>
     </BrowserRouter>
